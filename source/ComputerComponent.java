12de18b7f7f6fa28f5d689a1b3dcb0577d66f500
Leonid <leonidledn@gmail.com>
2025-04-17 11:50:24 -0500

    Add list and class for computer components

---
+++ source/ComputerComponent.java
@@ -0,0 +1,11 @@
+public class ComputerComponent {
+    public int id;
+    public double price;
+    public String type;
+
+    public ComputerComponent(double price, int id, String type) {
+        this.price = price;
+        this.id = id;
+        this.type = type;
+    }
+}
--- source/level1.java
+++ source/level1.java
@@ -4,14 +4,21 @@ import javax.swing.JPanel;
 
 public class Level1 extends LevelTemplate {
 
+    public ArrayList<ComputerComponent> components;
+
     public Level1(JPanel x, Window w) {
         super(x, w);
+        this.components = new ArrayList<ComputerComponent>();
     }
 
     @Override
     public void setLevelActive() {
         // Set the level active and perform any necessary initialization here
         System.out.println("Level 1 is now active.");
+    }
+
+    public ArrayList<ComputerComponent> getComponents() {
+        return components;
     }
 
 }
