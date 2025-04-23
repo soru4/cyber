14bf68e6d65d687671882351b7fbe18229924385
soru4 <73024932+soru4@users.noreply.github.com>
2025-04-23 13:00:19 -0500

    run errors Fixed 4-23

--- source/Scene.java
+++ source/Scene.java
@@ -1,14 +1,12 @@
-import java.awt.MouseInfo;
-import javax.swing.ImageIcon;
 
 class Scene {
 
     public Scene() {
         // add the classes that extend MonoBehaviour here( just call the constructor)
         GameLoop.__inst__.Instantiate(new TextCounter(), 0);
-        GameLoop.__inst__.Instantiate(
-                new MovableComponent(new ImageIcon("assets/75519.png"), MouseInfo.getPointerInfo().getLocation()), 0);
-        // new Level1(); 
+      //  GameLoop.__inst__.Instantiate(
+              //  new MovableComponent(new ImageIcon("assets/75519.png"), MouseInfo.getPointerInfo().getLocation()), 0);
+         new Level2(); 
     }
     public void runLevel2(){
         //Level2 mainGameWindow = new Level2();
