ffa5812851e61005ccda44f51346f93c3935314d
Leonid <leonidledn@gmail.com>
2025-05-02 11:32:40 -0500

    format, fix warnings

--- source/Scene.java
+++ source/Scene.java
@@ -3,11 +3,12 @@ class Scene {
 
     public Scene() {
         // add the classes that extend MonoBehaviour here( just call the constructor)
-        //GameLoop.__inst__.Instantiate(new TextCounter(), 0);
-      //  GameLoop.__inst__.Instantiate(
-              //  new MovableComponent(new ImageIcon("assets/75519.png"), MouseInfo.getPointerInfo().getLocation()), 0);
-         //new Level4();
-         //new Level();
-         Level0 start = new Level0();
+        // GameLoop.__inst__.Instantiate(new TextCounter(), 0);
+        // GameLoop.__inst__.Instantiate(
+        // new MovableComponent(new ImageIcon("assets/75519.png"),
+        // MouseInfo.getPointerInfo().getLocation()), 0);
+        // new Level4();
+        // new Level();
+        new Level0();
     }
 }
