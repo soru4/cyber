c2e88a33f85c80d28d45966951e1bd6f3f0ae847
soru4 <73024932+soru4@users.noreply.github.com>
2025-05-07 13:16:46 -0500

    level3good ish

--- source/Level3.java
+++ source/Level3.java
@@ -1,3 +1,4 @@
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
@@ -16,7 +17,13 @@ public class Level3 extends Level implements ActionListener, MouseListener, Mous
     public boolean DMZ = false;
     public boolean VPN = false;
     public boolean DHCP = false;
+    public boolean TCP = false;
+    public boolean UDP = false; 
 
+    public String port;
+    public String VPNsubnet;
+    public String ipString;
+    public String rangeString;
     Point startPoint;
     private static final int ROWS = 5;
     private static final int COLS = 2;
@@ -127,6 +134,11 @@ public class Level3 extends Level implements ActionListener, MouseListener, Mous
                     centerPanel.revalidate();
                     centerPanel.repaint();
                     JLabel ipLabel = new JLabel("DMZ Set Up:");
+                    ImageIcon i = new ImageIcon("assets/questionmark.png");
+                    JButton b = new JButton(i);
+                    b.addActionListener((ActionEvent e1) ->{
+                        JOptionPane.showMessageDialog(frame, "Source Google: A DMZ, or demilitarized zone, is a network or subnet that acts as a buffer zone between an organization's internal network and the untrusted external network (like the internet). It protects the internal network by isolating services that need to be accessed from the outside. ");
+                    });
                     ipLabel.setFont(new Font("Arial", Font.BOLD, 24));
                     // create button to set up DMZ
                     JButton ipButton = new JButton("Set Up DMZ Server and Client");
@@ -141,26 +153,39 @@ public class Level3 extends Level implements ActionListener, MouseListener, Mous
                     ipLabel.setFont(new Font("Arial", Font.BOLD, 24)); // set the font size
 
                     centerPanel.add(ipLabel);
+                    centerPanel.add(b);
                     centerPanel.add(ipButton);
                     System.out.println("Button clicked: " + e.getActionCommand());
                 });
             } else if (x.getText().equals("Open VPN Setup")) {
                 x.addActionListener((ActionEvent e) -> {
-
+                    
                     centerPanel.removeAll();
                     centerPanel.revalidate();
                     centerPanel.repaint();
                     JLabel ipLabel = new JLabel("OpenVPN:" + " Set Up VPN Server and Client");
 
+                    ImageIcon i = new ImageIcon("assets/questionmark.png");
+                    JButton b = new JButton(i);
+                    b.addActionListener((ActionEvent e1) ->{
+                        JOptionPane.showMessageDialog(frame, "Source Google: OpenVPN refers to an open-source VPN protocol and software that enables secure, encrypted connections over the internet. It's a way to create a virtual private network (VPN) for secure data transmission, often used to protect sensitive information and bypass censorship or geographic restrictions. ");
+                    });
+
+
                     ipLabel.setFont(new Font("Arial", Font.BOLD, 24));
                     JButton ipButton = new JButton("Set Up VPN Server and Client");
+                    centerPanel.add(b);
+                    ipButton.setSelected(VPN);
                     centerPanel.add(ipLabel);
                     centerPanel.add(ipButton);
 
                     JButton tcpButton = new JButton("TCP");
                     JButton udpButton = new JButton("UDP");
+                    tcpButton.setSelected(TCP);
+                    udpButton.setSelected(UDP);
 
                     JTextField portField = new JTextField(10);
+                    portField.setText(port);
                     JPanel portPanel = new JPanel();
                     portPanel.add(new JLabel("Port Number:"));
                     portPanel.add(portField);
@@ -171,21 +196,24 @@ public class Level3 extends Level implements ActionListener, MouseListener, Mous
                     JTextField subnetField = new JTextField(10);
                     JPanel subnetPanel = new JPanel();
                     subnetPanel.add(new JLabel("VPN Subnet Mask:"));
+                    subnetField.setText(VPNsubnet);
                     subnetPanel.add(subnetField);
                     centerPanel.add(subnetPanel);
 
                     ipButton.addActionListener((ActionEvent e2) -> {
 
-                        if ((tcpButton.isSelected() || udpButton.isSelected()) && !portField.getText().isEmpty()
-                                && !subnetField.getText().isEmpty()) {
-                            String protocol = tcpButton.isSelected() ? "TCP" : "UDP";
-                            String port = portField.getText();
-                            String subnet = subnetField.getText();
+                        if ((tcpButton.isSelected() ) && !portField.getText().isEmpty() && !subnetField.getText().isEmpty()) {
+                            this.port = portField.getText();
+                            this.VPNsubnet = subnetField.getText();
+                           
                             VPN = true;
-                            System.out.println("VPN Server and Client Set Up with Protocol: " + protocol + ", Port: "
-                                    + port + ", Subnet: " + subnet);
-                            JOptionPane.showMessageDialog(frame, "VPN Server and Client Set Up with Protocol: "
-                                    + protocol + ", Port: " + port + ", Subnet: " + subnet);
+                           
+                        } else if ((udpButton.isSelected() ) && !portField.getText().isEmpty() && !subnetField.getText().isEmpty()) {
+                            this.port = portField.getText();
+                            this.VPNsubnet = subnetField.getText();
+                           
+                            VPN = true;
+                           
                         } else {
                             System.out.println("Please select a protocol (TCP or UDP)");
                         }
@@ -202,10 +230,19 @@ public class Level3 extends Level implements ActionListener, MouseListener, Mous
                     centerPanel.revalidate();
                     centerPanel.repaint();
                     JLabel ipLabel = new JLabel("DHCP Set Up:" + "");
+                    ImageIcon i = new ImageIcon("assets/questionmark.png");
+                    JButton b = new JButton(i);
+                    b.addActionListener((ActionEvent e1) ->{
+                        JOptionPane.showMessageDialog(frame, "Source Google: DHCP, or Dynamic Host Configuration Protocol, is a network protocol that automatically assigns IP addresses and other network configuration information to devices on a network. It simplifies IP management by automating the process of assigning IP addresses to devices, reducing manual configuration efforts. ");
+                    });
+
                     ipLabel.setFont(new Font("Arial", Font.BOLD, 24)); // set the font size
                     centerPanel.add(ipLabel);
+                    centerPanel.add(b);
                     JTextField clientIpField = new JTextField(10);
+                    clientIpField.setText(ipString);
                     JTextField rangeField = new JTextField(10);
+                    rangeField.setText(rangeString);
                     JPanel dhcpPanel = new JPanel();
                     dhcpPanel.add(new JLabel("Client IP Address:"));
                     dhcpPanel.add(clientIpField);
@@ -213,8 +250,11 @@ public class Level3 extends Level implements ActionListener, MouseListener, Mous
                     dhcpPanel.add(rangeField);
                     JButton dhcpButton = new JButton("Set Up DHCP Server and Client");
                     dhcpButton.addActionListener((ActionEvent e2) -> {
-                        if (!clientIpField.getText().isEmpty() && !rangeField.getText().isEmpty())
+                        if (!clientIpField.getText().isEmpty() && !rangeField.getText().isEmpty()){
+                            ipString = clientIpField.getText();
+                            rangeString = rangeField.getText();
                             DHCP = true;
+                        }
                         System.out.println("DHCP Server and Client Set Up with Client IP: " + clientIpField.getText()
                                 + " and Range: " + rangeField.getText());
                         JOptionPane.showMessageDialog(frame, "DHCP Server and Client Set Up with Client IP: "
